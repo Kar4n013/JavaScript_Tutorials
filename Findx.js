0002002f1e8f5ab9247a2f921067930623fd7166
Kar4n013 <karangiri01304@gmail.com>
2026-05-26 14:58:23 +0530

    Added Findx

--- Findx.js
+++ Findx.js
@@ -1,4 +1,6 @@
 //Find number of times a digit occurs in given input
 const n1 = 98790959
-let n2 = (n1.toString().match(/9/g) || []).length;
-console.log(n2);
+const target = 9
+let regex = new RegExp(target,"g");
+let n2 = (n1.toString().match(regex) || []).length;
+console.log(target+" is repeated "+n2+" times in given input "+n1);
